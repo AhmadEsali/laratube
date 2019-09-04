148d1cfddc7e88c0b126ed15ec7c088500dfce40
AhmadEsali <madrista26@gmail.com>
2019-09-04 14:59:16 +0300

    setup and make db seed

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\User;
+use App\Channel;
+use App\Subscription;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user1 = factory(User::class)->create([
+            'email' => 'john@doe.com'
+        ]);
+
+        $user2 = factory(User::class)->create([
+            'email' => 'jane@doe.com'
+        ]);
+
+        $channel1 = factory(Channel::class)->create([
+            'user_id' => $user1->id
+        ]);
+
+        $channel2 = factory(Channel::class)->create([
+            'user_id' => $user2->id
+        ]);
+
+        $channel1->subscriptions()->create([
+            'user_id' => $user2->id,
+
+        ]);
+
+        $channel2->subscriptions()->create([
+            'user_id' => $user1->id,
+
+        ]);
+       
+
+        factory(Subscription::class,10000)->create([
+            'channel_id' => $channel1->id
+        ]);
+
+        factory(Subscription::class,10000)->create([
+            'channel_id' => $channel2->id
+        ]);
+    }
+}
